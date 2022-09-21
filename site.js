de38d364013c6340be4bbddad170a73e3c36198b
thidoa <thiagovini200@gmail.com>
2022-09-21 20:48:25 -0300

    add o campo skils e um projeto

--- site.js
+++ site.js
@@ -42,3 +42,69 @@ const mobileNavbar = new MobileNavbar(
     ".nav-list li",
 )
 mobileNavbar.init()
+
+
+window.onscroll = function() {myFunction()};
+
+var valor = document.getElementById("introduction");
+var navbar = document.getElementById("navbar");
+var sticky = valor.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+// function escrever(elemento) {
+//     const textoArray = elemento.innerHTML.split('');
+//     elemento.innerHTML = '';
+//     textoArray.forEach((letra, i) => {
+//         setTimeout(() => {
+//             elemento.innerHTML += letra
+//         }, 75 * i);
+//     });
+// }
+
+// const titulo = document.querySelector('.saida');
+// //escrever(titulo);
+
+document.addEventListener('DOMContentLoaded', () => {
+    
+    
+    new TypeIt('.animated', {
+        speed: 90,
+        loop: true
+    }).pause(1500).go()
+
+    setTimeout(function(){
+        document.querySelector('#code-rigth')
+            .classList.toggle('code-color-final'),
+        document.querySelector('#code-left')
+            .classList.toggle('code-color-final')
+    },1800);
+
+    setTimeout(function(){
+        document.querySelector('#resumo')
+             .classList.toggle('resumo-final'),
+        document.querySelector('#linha')
+             .classList.toggle('linha-final'),
+        document.querySelector('#contato')
+            .classList.toggle('contato-final'),
+        document.querySelector('#navbar')
+            .classList.toggle('menu-final'),
+        document.querySelector('#icon-seta')
+            .classList.toggle('icon-seta-final'),
+        navbar.classList.add("::-webkit-scrollbar-teste")
+    },2200);
+
+    setTimeout(function(){
+        document.querySelector('#header')
+            .classList.toggle('header-final')
+    },3000);
+})
+
+
+
